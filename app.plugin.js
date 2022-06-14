921e2453c35838e87daae946566564daacafdf9e
青衣沽酒 <982039620@qq.com>
2022-06-14 19:52:42 +0800

    完成react-native0.68版本支持

--- app.plugin.js
+++ app.plugin.js
@@ -39,7 +39,7 @@ const setAppDelegate = (config) =>
     }
     if (
       config.modResults.contents.indexOf(
-        'JPUSHService setupWithOption:launchOptions'
+        'JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];'
       ) === -1
     ) {
       console.log(
@@ -61,107 +61,95 @@ const setAppDelegate = (config) =>
           0,
           didFinishLaunchingWithOptionsStartIndex
         ) +
-        `  // JPush初始化配置
-  [JPUSHService setupWithOption:launchOptions appKey:@"${JPUSH_APPKEY}" channel:@"${JPUSH_CHANNEL}" apsForProduction:YES];
-  // APNS
-  JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
-  if (@available(iOS 12.0, *)) {
-    entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound|JPAuthorizationOptionProvidesAppNotificationSettings;
-  }
-  [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
-  [launchOptions objectForKey: UIApplicationLaunchOptionsRemoteNotificationKey];
-  // 自定义消息
-  NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
-  [defaultCenter addObserver:self selector:@selector(networkDidReceiveMessage:) name:kJPFNetworkDidReceiveMessageNotification object:nil];
-  // 地理围栏
-  [JPUSHService registerLbsGeofenceDelegate:self withLaunchOptions:launchOptions];
-#if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
-  InitializeFlipper(application);
-#endif
+        `  
+        // APNS
+        JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
+        if (@available(iOS 12.0, *)) {
+          entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound|JPAuthorizationOptionProvidesAppNotificationSettings;
+        }
+        [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+        [launchOptions objectForKey: UIApplicationLaunchOptionsRemoteNotificationKey];
+        // 自定义消息
+        NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+        [defaultCenter addObserver:self selector:@selector(networkDidReceiveMessage:) name:kJPFNetworkDidReceiveMessageNotification object:nil];
+        // 地理围栏
+        [JPUSHService registerLbsGeofenceDelegate:self withLaunchOptions:launchOptions];
 ` +
         config.modResults.contents.slice(
           didFinishLaunchingWithOptionsStartIndex
         );
-    } else {
-      console.log(
-        '\n[JPushExpoConfigPlugin] 配置 AppDelegate appKey & channel ... '
-      );
-      config.modResults.contents = config.modResults.contents.replace(
-        /appKey\:\@\"(.*)\" channel\:\@\"(.*)\" /,
-        `appKey:@"${JPUSH_APPKEY}" channel:@"${JPUSH_CHANNEL}" `
-      );
     }
     if (
       config.modResults.contents.indexOf(
-        '// ************************************************JPush start************************************************'
+        'JPush start'
       ) === -1
     ) {
       console.log('\n[JPushExpoConfigPlugin] 配置 AppDelegate other ... ');
       config.modResults.contents = config.modResults.contents.replace(
         /\@end([\n]*)$/,
-        `// ************************************************JPush start************************************************
+        `//************************************************JPush start************************************************
 
-//注册 APNS 成功并上报 DeviceToken
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-  [JPUSHService registerDeviceToken:deviceToken];
-}
-
-// iOS 7 APNS
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:  (NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-  // iOS 10 以下 Required
-  NSLog(@"iOS 7 APNS");
-  [JPUSHService handleRemoteNotification:userInfo];
-  [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo];
-  completionHandler(UIBackgroundFetchResultNewData);
-}
-
-// iOS 10 前台收到消息
-- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center  willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
-  NSDictionary * userInfo = notification.request.content.userInfo;
-  if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
-    // Apns
-    NSLog(@"iOS 10 APNS 前台收到消息");
-    [JPUSHService handleRemoteNotification:userInfo];
-    [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo];
-  }
-  else {
-    // 本地通知 todo
-    NSLog(@"iOS 10 本地通知 前台收到消息");
-    [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_ARRIVED_EVENT object:userInfo];
-  }
-  //需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
-  completionHandler(UNNotificationPresentationOptionAlert);
-}
-
-// iOS 10 消息事件回调
-- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler: (void (^)(void))completionHandler {
-  NSDictionary * userInfo = response.notification.request.content.userInfo;
-  if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
-    // Apns
-    NSLog(@"iOS 10 APNS 消息事件回调");
-    [JPUSHService handleRemoteNotification:userInfo];
-    // 保障应用被杀死状态下，用户点击推送消息，打开app后可以收到点击通知事件
-    [[RCTJPushEventQueue sharedInstance]._notificationQueue insertObject:userInfo atIndex:0];
-    [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_OPENED_EVENT object:userInfo];
-  }
-  else {
-    // 本地通知
-    NSLog(@"iOS 10 本地通知 消息事件回调");
-    // 保障应用被杀死状态下，用户点击推送消息，打开app后可以收到点击通知事件
-    [[RCTJPushEventQueue sharedInstance]._localNotificationQueue insertObject:userInfo atIndex:0];
-    [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_OPENED_EVENT object:userInfo];
-  }
-  // 系统要求执行这个方法
-  completionHandler();
-}
-
-// 自定义消息
-- (void)networkDidReceiveMessage:(NSNotification *)notification {
-  NSDictionary * userInfo = [notification userInfo];
-  [[NSNotificationCenter defaultCenter] postNotificationName:J_CUSTOM_NOTIFICATION_EVENT object:userInfo];
-}
-
-// ************************************************JPush end************************************************
+    //注册 APNS 成功并上报 DeviceToken
+    - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+      [JPUSHService registerDeviceToken:deviceToken];
+    }
+    
+    //iOS 7 APNS
+    - (void)application:(UIApplication *)application didReceiveRemoteNotification:  (NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+      // iOS 10 以下 Required
+      NSLog(@"iOS 7 APNS");
+      [JPUSHService handleRemoteNotification:userInfo];
+      [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo];
+      completionHandler(UIBackgroundFetchResultNewData);
+    }
+    
+    //iOS 10 前台收到消息
+    - (void)jpushNotificationCenter:(UNUserNotificationCenter *)center  willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
+      NSDictionary * userInfo = notification.request.content.userInfo;
+      if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        // Apns
+        NSLog(@"iOS 10 APNS 前台收到消息");
+        [JPUSHService handleRemoteNotification:userInfo];
+        [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_ARRIVED_EVENT object:userInfo];
+      }
+      else {
+        // 本地通知 todo
+        NSLog(@"iOS 10 本地通知 前台收到消息");
+        [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_ARRIVED_EVENT object:userInfo];
+      }
+      //需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
+      completionHandler(UNNotificationPresentationOptionAlert);
+    }
+    
+    //iOS 10 消息事件回调
+    - (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler: (void (^)(void))completionHandler {
+      NSDictionary * userInfo = response.notification.request.content.userInfo;
+      if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        // Apns
+        NSLog(@"iOS 10 APNS 消息事件回调");
+        [JPUSHService handleRemoteNotification:userInfo];
+        // 保障应用被杀死状态下，用户点击推送消息，打开app后可以收到点击通知事件
+        [[RCTJPushEventQueue sharedInstance]._notificationQueue insertObject:userInfo atIndex:0];
+        [[NSNotificationCenter defaultCenter] postNotificationName:J_APNS_NOTIFICATION_OPENED_EVENT object:userInfo];
+      }
+      else {
+        // 本地通知
+        NSLog(@"iOS 10 本地通知 消息事件回调");
+        // 保障应用被杀死状态下，用户点击推送消息，打开app后可以收到点击通知事件
+        [[RCTJPushEventQueue sharedInstance]._localNotificationQueue insertObject:userInfo atIndex:0];
+        [[NSNotificationCenter defaultCenter] postNotificationName:J_LOCAL_NOTIFICATION_OPENED_EVENT object:userInfo];
+      }
+      // 系统要求执行这个方法
+      completionHandler();
+    }
+    
+    //自定义消息
+    - (void)networkDidReceiveMessage:(NSNotification *)notification {
+      NSDictionary * userInfo = [notification userInfo];
+      [[NSNotificationCenter defaultCenter] postNotificationName:J_CUSTOM_NOTIFICATION_EVENT object:userInfo];
+    }
+    
+    //************************************************JPush end************************************************
 
 @end
 `
